d4436d557531aa5d4fb616388a3bb352fe88bd35
Mark-Daniels <github.markdaniels@gmail.com>
2016-05-02 16:15:32 -0700

    add form data to database

--- server/config/routes.js
+++ server/config/routes.js
@@ -4,4 +4,8 @@ module.exports = function(app) {
 	app.get('/friends', function(req, res) {
 		friends.index(req, res);
 	});
+
+	app.post('/friends', function(req, res){
+		friends.create(req, res);
+	});
 };
--- server/controllers/friends.js
+++ server/controllers/friends.js
@@ -11,6 +11,22 @@ module.exports = (function() {
 					res.json(results);
 				}
 			})
+		},
+
+		create: function(req, res) {
+			console.log("POST DATA", req.body);
+
+			if (req.body.name) {
+				var newFriend = new Friend({ name: req.body.name, age: req.body.age });
+				newFriend.save(function(err, results) {
+					if (err) {
+						console.log('Something went wrong', err);
+					} else {
+						console.log('Successfully added new friend!');
+						res.json(results);
+					}
+				});
+			}
 		}
 	}
 })();
